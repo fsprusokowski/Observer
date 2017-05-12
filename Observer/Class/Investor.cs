71d95b051a5596f06f983e8923ee203e1cd4bd83
fsprusokowski <prusokowski@hotmail.com>
2017-05-12 10:24:03 -0300

    Removed unnecessary  includes.

--- Observer/Class/Investor.cs
+++ Observer/Class/Investor.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Observer.Class
+﻿namespace Observer.Class
 {
     public abstract class Investor
     {
